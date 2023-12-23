02623e51634ae8652c9a2e5d0088a5c30f43e655
Arzaq Doudar <arzaqziad20@gmail.com>
2023-12-23 16:31:41 +0200

    code cleanup for app.js

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ import DocsRouter from "./routes/docs.routes.js";
 import {expressjwt} from "express-jwt";
 import {jwtPassword} from './constants/login.constants.js';
 import { userMiddleware } from './middleware/user.middleware.js';
-//import module from './apiweather.js';
-//import apiweather from './apiweather.js';
-//import getWeatherData from './weatherApi';
+
 const app = express();
 
 app.use(express.json());
@@ -107,61 +105,5 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.send(err)
 });
-
-
-////////////////////
-
-//const api = require('./apiweather.js')
-
-// app.js
-// // weatherApi.js
-// const axios = require('axios');
-
-// const getWeatherData = async () => {
-//   const options = {
-//     method: 'GET',
-//     url: 'https://tomorrow-io1.p.rapidapi.com/v4/weather/forecast',
-//     params: {
-//       location: '42.15,82.1',
-//       timesteps: '1h',
-//       units: 'metric'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': '395b7cace2msh03c0453c05785d9p19b29djsn80ef7b724a99',
-//       'X-RapidAPI-Host': 'tomorrow-io1.p.rapidapi.com'
-//     }
-//   };
-
-//   try {
-//     const response = await axios.request(options);
-//     console.log(response.data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-// export default getWeatherData;
-// //module.exports = getWeatherData;
-
-
-// const express = require("express");
-// const axios = require("axios");
-
-// const app = express();
-
-// app.get("/forcast", async (req, res, next) => {
-    
- 
-// }
-
-
-
-// app.listen(3000, () => {
-//   console.log("Connected");
-// });
-///////////////////
-
-
-
-
 
 export default app;
