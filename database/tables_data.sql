327bf05564e8402ed0adbf000111740bf33398ef
Arzaq Doudar <arzaqziad20@gmail.com>
2023-12-23 23:55:47 +0200

    add data for community_report table
    in table_data file

--- database/tables_data.sql
+++ database/tables_data.sql
@@ -9,4 +9,13 @@ VALUES
   ('Climate Change Impacts on Ecosystems', 'Climate Change', 'Article', 'Michael Brown', '2023-07-01', 'https://example.com/path/to/climatechange.pdf'),
   ('Energy-Efficient Home Practices', 'Energy', 'Guide', 'Olivia Taylor', '2023-08-10', 'https://example.com/path/to/energyefficient.pdf');
 
-  
+INSERT INTO community_report (user_id, report_type, description, location, time_stamp)
+VALUES
+  (1, 'Pollution', 'Air pollution in the city center', 'City Center', '2023-01-01T12:00:00'),
+  (1, 'Accident', 'Car accident on Main Street', 'Main Street', '2023-01-02T14:30:00'),
+  (1, 'Vandalism', 'Graffiti on public property', 'Downtown Area', '2023-01-04T15:45:00'),
+  (1, 'Traffic Jam', 'Heavy traffic congestion on Highway 101', 'Highway 101', '2023-01-05T08:30:00'),
+  (1, 'Fire Hazard', 'Unattended fire in the open field', 'Open Field', '2023-01-07T17:20:00'),
+  (2, 'Noise', 'Loud construction noise near the park', 'Park Area', '2023-01-03T09:00:00'),
+  (2, 'Garbage', 'Overflowing trash bins in the neighborhood', 'Residential Area', '2023-01-06T11:00:00'),
+  (2, 'Street Lighting Issue', 'Street lights not working on Elm Street', 'Elm Street', '2023-01-08T19:15:00');
