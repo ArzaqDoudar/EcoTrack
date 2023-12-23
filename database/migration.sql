fe3ac7529185f7626fb879ec00ba73e1a9ec9be4
Arzaq Doudar <arzaqziad20@gmail.com>
2023-12-23 16:35:58 +0200

    edit db migration

--- database/migration.sql
+++ database/migration.sql
@@ -9,8 +9,9 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
   `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL ,
   `location` varchar(1024) DEFAULT NULL,
+  `score` int NOT NULL DEFAULT 0,
   PRIMARY KEY (`Id`),
   UNIQUE KEY `UserName_UNIQUE` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
