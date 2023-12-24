bc128fb185d753fc24357e1cfba903476b4e1a5e
Arzaq Doudar <arzaqziad20@gmail.com>
2023-12-25 00:03:13 +0200

    add user role to the token

--- utils/token.utils.js
+++ utils/token.utils.js
@@ -4,7 +4,7 @@ import {jwtPassword} from "../constants/login.constants.js";
 export const generateToken = async (user) => {
     const currentTime = Math.floor(Date.now() / 1000);
     return jwt.sign(
-        {username: user.username, location: user.location, name: user.name, role: user.role, iat: currentTime },
+        {username: user.username, location: user.location, name: user.name, role: user.user_role, iat: currentTime },
         jwtPassword,
         {algorithm: 'HS256', expiresIn: '1d'}
     );
